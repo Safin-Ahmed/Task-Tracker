a8aede90807f1ad99e07d7dfb9fcff62c134ee1f
Safin-Ahmed <safin.ahmed2000@gmail.com>
2024-01-05 14:15:15 +0600

    added eas update support

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
-import { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import TaskListScreen from "./screens/TaskListScreen";
 import { TasksProvider } from "./context/TasksContext";
 
+import * as Updates from "expo-updates";
+import { useEffect } from "react";
+
 const Stack = createNativeStackNavigator();
 
+async function onFetchUpdateAsync() {
+  try {
+    const update = await Updates.checkForUpdateAsync();
+
+    if (update.isAvailable) {
+      await Updates.fetchUpdateAsync();
+      await Updates.reloadAsync();
+    }
+  } catch (error) {
+    alert(`Error fetching latest Expo update: ${error}`);
+  }
+}
+
 export default function App() {
+  useEffect(() => {
+    onFetchUpdateAsync();
+  }, []);
+
   return (
     <TasksProvider>
       <NavigationContainer>
